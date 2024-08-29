921cc4015e8fa76aee9f0ce442894306da0db461
aviraj0403 <aviraj0403@gmail.com>
2024-08-30 00:50:16 +0530

    port

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 
 dbconnect();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
